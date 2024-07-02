34d90960c855ec595e7fa1894c6835efa260f455
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2024-07-02 16:33:20 +0100

    Feat:SignUp flow completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello i am Dolapo</div>;
+  return <div className="App">
+    {/* hello i am Dolapo  ------> NAVBAR */}
+    {/* hello i am kelvin ---> SING UP FLOW */}
+
+
+
+  </div>;
 }
 
 export default App;
