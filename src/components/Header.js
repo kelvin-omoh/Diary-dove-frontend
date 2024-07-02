9615c789ce70519e6ead100710bbf0aa9ba15e3c
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2024-07-02 22:54:14 +0100

    feat:Header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import logo from '../assets/DiaraDove Logo.png'
+import Bell from '../assets/Notification 2.png'
+import { BsBell, BsBellSlash } from 'react-icons/bs'
+import { AiFillSetting } from 'react-icons/ai'
 const Header = () => {
     return (
-        <div className=' text-red-500 '>
+        <div className='  border-b-[1px] border-[#B4B9C2]  flex justify-between   px-[80px] py-[18px]  '>
 
-
+            <div>
+                <img className=' w-[146px] h-[36px]' src={logo} alt={'logo'} />
+            </div>
+            <div className=' flex items-center gap-[19.8px]'>
+                <img className=' h-[22.25px] ' src={Bell} alt={'logo'} />
+                <AiFillSetting className='text-[#B4B9C2] ' size={20} />
+            </div>
         </div>
     )
 }
