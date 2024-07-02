6f9e9234b733dd4746edd2d3eec7389c79c3a769
Ifedol <dolapomuizat@gmail.com>
2024-07-02 16:56:49 +0100

    t

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    {/* hello i am Dolapo  ------> NAVBAR */}
-    {/* hello i am kelvin ---> SING UP FLOW */}
-
-
-
-  </div>;
+  return (
+    <div className="App">
+      {/* hello i am Dolapo  ------> NAVBAR */}
+      {/* hello i am kelvin ---> SING UP FLOW */}
+      <h1>Navbar</h1>
+    </div>
+  );
 }
 
 export default App;
