ab85949b1761c465fc5ce8706bbc74f2b17a18ca
Ifedol <dolapomuizat@gmail.com>
2024-07-05 11:26:04 +0100

    feat: login

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SetUp from "./pages/SetUp";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Toaster position="top-right" reverseOrder={false} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/setup" element={<SetUp />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className=" w-full flex">
+      <div className=" bg-gradient-to-br  from-[#DA9658] to-[#91643B] h-[1024px] w-[660px]"></div>
+      <div className=" mx-auto w-[780px] pt-[20px] ">
+        <h1 className=" font-[600] text-[32px]">Create Account</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
