968d6927daa9feb4976b6b293795c32240959a11
Enaikele Odion <enaikeleodion@gmail.com>
2024-07-22 11:19:43 +0100

    prev code

--- src/components/_LandingPage/Header.js
+++ src/components/_LandingPage/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import logo from '../../assets/DiaraDove Logo.png';
 import { useNavigate } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
@@ -37,12 +37,13 @@ const customScroll = (el) => {
 
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
+    const focusedLinkRef = useRef(null);
+    const navigate = useNavigate();
 
     const links = [
-        { name: 'Services', path: '#services' },
-        { name: 'How it works', path: '#how-it-works' },
-        { name: 'Contact Us', path: '#footer' },
+        { name: 'Services', path: '/' },
+        { name: 'How it works', path: '/se ' },
+        { name: 'Contact Us', path: '/footer' },
     ];
 
 
@@ -51,8 +52,50 @@ const Header = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     };
 
+    useEffect(() => {
+        if (focusedLinkRef.current) {
+            focusedLinkRef.current.focus();
+            focusedLinkRef.current = null; // Reset the ref after focusing
+        }
+    }, [focusedLinkRef]);
 
-    const navigate = useNavigate();
+    const handleScroll = (id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            const offset = -90; // Adjust this value for your desired offset
+            const elementPosition = element.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset + offset;
+            smoothScrollTo(offsetPosition);
+            setIsMobileMenuOpen(false); // Close the menu after clicking a link
+
+            // Focus on the link after scrolling
+            focusedLinkRef.current = id;
+        }
+    };
+
+    const smoothScrollTo = (targetPosition) => {
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 200; // Duration in milliseconds
+        let startTime = null;
+
+        const animation = (currentTime) => {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+
+        const ease = (t, b, c, d) => {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+
+        requestAnimationFrame(animation);
+    };
 
     return (
         <div className='z-10 fixed top-0 w-[100vw] flex flex-row items-center py-[16px] justify-between px-[24px] md:px-[80px] bg-white'>
@@ -64,7 +107,7 @@ const Header = () => {
             </button>
             <div className='hidden md:flex items-center justify-center'>
                 <ul className='flex gap-[56px] items-center'>
-                    {links.map(link => (
+                    {links.map((link) => (
                         <li key={link.name} onClick={() => setIsMobileMenuOpen(false)}>
                             <Link to={link.path} className='ease-in z-[50] relative transition-all duration-150 hover:text-[#dd9a5b]' >{link.name}</Link>
 
@@ -80,7 +123,7 @@ const Header = () => {
             {/* Mobile menu */}
             <div className={`md:hidden flex-col z-[50] py-[30px] absolute top-[65px] left-0 text-white w-full flex items-center justify-center transition-all bg-black/80 backdrop-blur-sm duration-300 ease-in-out ${isMobileMenuOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
                 <ul className='flex flex-col z-[50] relative justify-center gap-[20px] items-center'>
-                    {links.map(link => (
+                    {links.map((link) => (
                         <li key={link.name} onClick={() => setIsMobileMenuOpen(false)}>
                             <Link to={link.path} scroll={customScroll} className='ease-in z-[50] relative transition-all duration-150 hover:text-[#dd9a5b]'>
                                 {link.name}
